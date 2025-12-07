880d8bd6ef1edc0fa9821af9f7cda334c6333d2b
tufara18-png <tufara18@gmail.com>
2025-12-07 14:06:13 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  server: {
-    port: 3000,
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
